9e7995f7b284c6672e5448e9c317f6de67b90737
xuybin <xuybin@qq.com>
2022-04-17 18:52:25 +0800

    refactor:

--- esbuild.ts
+++ esbuild.ts
@@ -2,10 +2,10 @@
 import * as esbuildWasm from "https://deno.land/x/esbuild_deno@v0.0.1/wasm/browser.js";
 import * as esbuildNative from "https://deno.land/x/esbuild_deno@v0.0.1/native/mod.js";
 import { denoPlugin } from "https://deno.land/x/esbuild_deno@v0.0.1/mod.ts";
-
+const wasmURL = "https://deno.land/x/esbuild_deno@v0.0.1/wasm/esbuild.wasm";
 // @ts-ignore trust me
 const esbuild: typeof esbuildWasm = Deno.run === undefined
   ? esbuildWasm
   : esbuildNative;
 
-export { denoPlugin, esbuild, esbuildWasm as esbuildTypes };
+export { denoPlugin, esbuild, esbuildWasm as esbuildTypes, wasmURL };

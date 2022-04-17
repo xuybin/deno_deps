5cc7ab3b2d420cf80fbefd5707bc798d1ab72d45
xuybin <xuybin@qq.com>
2022-04-17 18:17:08 +0800

    refactor:

--- esbuild.ts
+++ esbuild.ts
@@ -1,3 +1,4 @@
+// @deno-types="https://deno.land/x/esbuild_deno@v0.0.1/native/mod.d.ts"
 import * as esbuildWasm from "https://deno.land/x/esbuild_deno@v0.0.1/wasm/browser.js";
 import * as esbuildNative from "https://deno.land/x/esbuild_deno@v0.0.1/native/mod.js";
 import { denoPlugin } from "https://deno.land/x/esbuild_deno@v0.0.1/mod.ts";
